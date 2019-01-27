822450e433a7d2a884d730a4fa32e063990434c4
Bence Dányi <bence@danyi.me>
2019-01-27 17:46:32 +0100

    make it more fancy

--- client/app.js
+++ client/app.js
@@ -26,6 +26,7 @@ function updateShadows(e) {
     x = (45 + Math.max(-45, Math.min(45, -e.gamma))) / 90 * window.innerWidth;
     y = (45 + Math.max(-45, Math.min(45, -e.beta))) / 90 * window.innerHeight * .75;
   }
+  document.body.style.background = 'radial-gradient(circle at ' + x + 'px ' + y + 'px, #fff, #ddd 100%)'
   letters.forEach(function(letter) {
     var rect = letter.getBoundingClientRect();
     var lx = (rect.right + rect.left) / 2;
@@ -33,20 +34,23 @@ function updateShadows(e) {
     var dx = x - lx;
     var dy = y - ly;
     var d = Math.sqrt(dx*dx + dy*dy);
+    dx /= d; dy /= d;
     var shadow = [];
-    var p = 10;
+    var l = 1000;
+    var h = 50;
+    var p = Math.max(5, Math.ceil(d / 70));
     for(var i = 0; i < p + 1; i++) {
       var n = i / p;
-      var opacity = (1 - Math.sqrt(n)) / 3;
-      var f = n * n / 5;
-      var blur = Math.min(2 + d * f, 30);
+      var opacity = (1 - Math.sqrt(n)) / 5 * Math.min(1, Math.max(.5, d / 100));
+      var f = n * (h * d) / (l - h);
+      var blur = (2 + n * 20) * Math.min(1, Math.max(.05, d / 500));
       shadow.push((dx * -f) + 'px ' + (dy * -f) + 'px ' + blur + 'px rgba(0,0,0,' + opacity + ')');
     }
     letter.style.textShadow = shadow.join(', ');
   });
 }
 
-updateShadows({pageX: window.innerWidth / 2, pageY: 100});
+updateShadows({pageX: window.innerWidth / 2, pageY: 0});
 
 document.addEventListener('mousemove', updateShadows, true);
-window.addEventListener('deviceorientation', updateShadows, true);
+// window.addEventListener('deviceorientation', updateShadows, true);
